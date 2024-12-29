e7c32a066ccdd258820af3afee65a162d7611862
NiPeGun <nipegun@gmail.com>
2024-12-29 01:52:42 +0100

    Update and rename IA-ModelosLLM-Instalar-LMStudio.sh to ModelosLLM-Instalar-LMStudio.sh

--- SoftInst/ParaCli/ModelosLLM-Instalar-LMStudio.sh
+++ SoftInst/ParaCli/ModelosLLM-Instalar-LMStudio.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar diferentes modelos LLM de Ollama en Debian
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/IA-ModelosLLM-Instalar-LMStudio.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/ModelosLLM-Instalar-LMStudio.sh | bash
 #
 # Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/IA-ModelosLLM-Instalar-LMStudio.sh | bash
+#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/ModelosLLM-Instalar-LMStudio.sh | bash
 # ----------
 
 # Indicar la cuenta de usuario no root
