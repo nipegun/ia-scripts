ca57e4c99f3e840b92227085a9d64156e48f3e3e
NiPeGun <nipegun@gmail.com>
2025-01-28 06:37:46 +0100

    Update Ollama-Qwen.sh

--- SoftInst/ParaCli/Modelos/Ollama-Qwen.sh
+++ SoftInst/ParaCli/Modelos/Ollama-Qwen.sh
@@ -9,10 +9,7 @@
 # Script de NiPeGun para instalar y configurar los diferentes modelos de Qwen en Ollama para Debian
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/ModelosLLM-Instalar-Ollama-Qwen.sh | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/ModelosLLM-Instalar-Ollama-Qwen.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Qwen.sh | bash
 # ----------
 
 # Definir constantes de color
