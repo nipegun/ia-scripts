bc3ab22fc18c41b53fb40c095dfa2cac02e74c4e
NiPeGun <nipegun@gmail.com>
2025-01-29 16:28:08 +0100

    Update Ollama-Dolphin-Uncensored.sh

--- SoftInst/ParaCli/Modelos/Ollama-Dolphin-Uncensored.sh
+++ SoftInst/ParaCli/Modelos/Ollama-Dolphin-Uncensored.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar los diferentes modelos de Dolphin-Mistral en Ollama para Debian
+# Script de NiPeGun para instalar y configurar los diferentes modelos de Dolphin Uncensored en Ollama para Debian
 #
 # Ejecución remota:
 #   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Dolphin-Mistral.sh | bash
@@ -47,20 +47,27 @@
   menu=(dialog --checklist "Marca los modelos que quieras instalar:" 22 96 16)
     opciones=(
 
-      1 "dolphin-mistral 7b-v2.8-q4_0 ( 4,3 GB en disco) ( 7,4 GB en VRAM)" off
-      2 "dolphin-mistral 7b-v2.8-q8_0 ( 7,9 GB en disco) (10,7 GB en VRAM)" off
-      3 "dolphin-mistral 7b-v2.8-fp16 (14,2 GB en disco) (15,4 GB en VRAM)" off
+      1 "dolphin-mistral 7b-v2.8-q4_0 ( 4,2 GB en disco) ( 5,1 GB en VRAM)" off
+      2 "dolphin-mistral 7b-v2.8-q8_0 ( 7,8 GB en disco) ( 8,6 GB en VRAM)" off
+      3 "dolphin-mistral 7b-v2.8-fp16 (14,3 GB en disco) (15,1 GB en VRAM)" off
 
-      4 "dolphin-mixtral 8x7b-v2.5-q8_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      5 "dolphin-mixtral 8x7b-v2.5-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      4 "dolphin-mixtral 8x7b-v2.6-q8_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      5 "dolphin-mixtral 8x7b-v2.6-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      4 "dolphin-mixtral 8x7b-v2.7-q8_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      5 "dolphin-mixtral 8x7b-v2.7-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
-      6 "dolphin-mixtral 8x22b-v2.9-q8_0 (xx,x GB en disco) (xx,x GB en VRAM)" off
-      7 "dolphin-mixtral 8x22b-v2.9-fp16 (xx,x GB en disco) (xx,x GB en VRAM)" off
+      4 "dolphin-mixtral 8x7b-v2.5-q4_0  (26,9 GB en disco) (xx,x GB en VRAM)" off
+      5 "dolphin-mixtral 8x7b-v2.5-q8_0  (49,2 GB en disco) (48,2 GB en VRAM)" off
+      6 "dolphin-mixtral 8x7b-v2.5-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
 
-      7 "dolphin-phi 2.7b-v2.6-q8_0 (xx,x GB en disco) (xx,x GB en VRAM)" off
+      7 "dolphin-mixtral 8x7b-v2.6-q4_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
+      8 "dolphin-mixtral 8x7b-v2.6-q8_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
+      9 "dolphin-mixtral 8x7b-v2.6-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
+
+     10 "dolphin-mixtral 8x7b-v2.7-q4_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
+     11 "dolphin-mixtral 8x7b-v2.7-q8_0  (xx,x GB en disco) (xx,x GB en VRAM)" off
+     12 "dolphin-mixtral 8x7b-v2.7-fp16  (xx,x GB en disco) (xx,x GB en VRAM)" off
+
+     13 "dolphin-mixtral 8x22b-v2.9-q4_0 (xx,x GB en disco) (xx,x GB en VRAM)" off
+     14 "dolphin-mixtral 8x22b-v2.9-q8_0 (xx,x GB en disco) (xx,x GB en VRAM)" off
+     15 "dolphin-mixtral 8x22b-v2.9-fp16 (xx,x GB en disco) (xx,x GB en VRAM)" off
+
+     16 "dolphin-phi 2.7b-v2.6-q8_0 (xx,x GB en disco) (xx,x GB en VRAM)" off
 
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
@@ -76,7 +83,7 @@
             echo ""
 
             # Definir el espacio libre necesario
-              vGBsLibresNecesarios=4.3
+              vGBsLibresNecesarios=4.2
               vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
 
             # Obtener el espacio libre en la partición raíz en kilobytes
@@ -103,7 +110,7 @@
             echo ""
 
             # Definir el espacio libre necesario
-              vGBsLibresNecesarios=7.9
+              vGBsLibresNecesarios=7.8
               vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
 
             # Obtener el espacio libre en la partición raíz en kilobytes
@@ -130,7 +137,7 @@
             echo ""
 
             # Definir el espacio libre necesario
-              vGBsLibresNecesarios=14.2
+              vGBsLibresNecesarios=14.3
               vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
 
             # Obtener el espacio libre en la partición raíz en kilobytes
@@ -143,6 +150,249 @@
               else
                 echo ""
                 echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mistral:7b-v2.8-fp16.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          4)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.5-q8_0..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.5-q8_0
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.5-q8_0.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          5)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.5-fp16..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.5-fp16
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.5-fp16.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          6)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.6-q8_0..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.6-q8_0
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.6-q8_0.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          7)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.6-fp16..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.6-fp16
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.6-fp16.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          8)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.7-q8_0..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.7-q8_0
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.7-q8_0.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+          9)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x7b-v2.7-fp16..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x7b-v2.7-fp16
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x7b-v2.7-fp16.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+         10)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x22b-v2.9-q8_0..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x22b-v2.9-q8_0
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x22b-v2.9-q8_0.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+         11)
+
+            echo ""
+            echo "  Instalando dolphin-mixtral:8x22b-v2.9-fp16..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-mixtral:8x22b-v2.9-fp16
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-mixtral:8x22b-v2.9-fp16.${cFinColor}"
+                echo ""
+                echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
+                echo ""
+              fi
+
+          ;;
+
+         12)
+
+            echo ""
+            echo "  dolphin-phi:2.7b-v2.6-q8_0..."
+            echo ""
+
+            # Definir el espacio libre necesario
+              vGBsLibresNecesarios=x
+              vEspacioNecesario=$(($vGBsLibresNecesarios * 1024 * 1024)) # Convertir a kilobytes (1GB = 1048576KB)
+
+            # Obtener el espacio libre en la partición raíz en kilobytes
+              vEspacioLibre=$(df / | grep '/' | tail -1 | sed -E 's/\s+/ /g' | cut -d ' ' -f 4)
+              vGBsLibres=$(echo "scale=2; $vEspacioLibre/1024/1024" | bc)
+
+            # Comprobar si hay espacio libre disponible
+              if [ "$vEspacioLibre" -ge "$vEspacioNecesario" ]; then
+                ollama pull dolphin-phi:2.7b-v2.6-q8_0
+              else
+                echo ""
+                echo -e "${cColorRojo}    No hay suficiente espacio libre para instalar el modelo dolphin-phi:2.7b-v2.6-q8_0.${cFinColor}"
                 echo ""
                 echo -e "${cColorRojo}      Hacen falta $vGBsLibresNecesarios GB y hay sólo $vGBsLibres GB.${cFinColor}"
                 echo ""
