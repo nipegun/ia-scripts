651da2dc3221ce7af812e66972cd90066c65b7b4
NiPeGun <nipegun@gmail.com>
2025-02-20 16:12:09 +0100

    Update LlamaCPP-Instalar.sh

--- SoftInst/ParaCli/LlamaCPP-Instalar.sh
+++ SoftInst/ParaCli/LlamaCPP-Instalar.sh
@@ -22,12 +22,12 @@
   cmake --build . --config Release
 
 # Crear carpeta
-  mkdir -p ~/Soft/IA/LlamaCPP/
-  cp ~/repos/ia/llama.cpp/build/bin/* ~/Soft/IA/LlamaCPP/
+  mkdir -p ~/IA/LlamaCPP/
+  cp ~/repos/ia/llama.cpp/build/bin/* ~/IA/LlamaCPP/
 
 # Descargar modelos
-  mkdir -p ~/Soft/IA/Modelos/GGUF/
-  cd ~/Soft/IA/Modelos/GGUF/
+  mkdir -p ~/IA/Modelos/GGUF/
+  cd ~/IA/Modelos/GGUF/
   curl -L https://huggingface.co/lmstudio-community/Llama-3.2-3B-Instruct-GGUF/resolve/main/Llama-3.2-3B-Instruct-Q8_0.gguf -O
 
 # Notificar fin de ejecución del script
@@ -36,23 +36,23 @@
   echo ""
   echo "    Para ejecutar llama.cpp y realizar una consulta, cerrando la conversación:"
   echo ""
-  echo "      ~/Soft/IA/LlamaCPP/llama-cli -m ~/Soft/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -p 'Hazme un script de python que diga hola?' -no-cnv"
+  echo "      ~/IA/LlamaCPP/llama-cli -m ~/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -p 'Hazme un script de python que diga hola?' -no-cnv"
   echo ""
   echo "      Podemos hacer que cierre la conversación, aunque no responda por completo, limitando el nro de tokens de respuesta:"
   echo ""
-  echo "        ~/Soft/IA/LlamaCPP/llama-cli -m ~/Soft/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -p 'Hazme un script de python que diga hola?' -n 128 -no-cnv"
+  echo "        ~/IA/LlamaCPP/llama-cli -m ~/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -p 'Hazme un script de python que diga hola?' -n 128 -no-cnv"
   echo ""
   echo "    Para ejecutar en modo conversación:"
   echo ""
-  echo "      ~/Soft/IA/LlamaCPP/llama-cli -m ~/Soft/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf"
+  echo "      ~/IA/LlamaCPP/llama-cli -m ~/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf"
   echo ""
   echo "    Para ejecutar como API/servidor:"
   echo ""
-  echo "      ~/Soft/IA/LlamaCPP/llama-server --port 9000 -m ~/Soft/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf"
+  echo "      ~/IA/LlamaCPP/llama-server --port 9000 -m ~/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf"
   echo ""
   echo "      Crear un servidor para 4 usuarios simultáneos y contexto de 4096 para cada uno:"
   echo ""
-  echo "        ~/Soft/IA/LlamaCPP/llama-server --port 9000 -m ~/Soft/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -c 16384 -np 4"
+  echo "        ~/IA/LlamaCPP/llama-server --port 9000 -m ~/IA/Modelos/GGUF/Llama-3.2-3B-Instruct-Q8_0.gguf -c 16384 -np 4"
   echo ""
   echo "      Luego podemos tirarle consultas con:"
   echo ""
