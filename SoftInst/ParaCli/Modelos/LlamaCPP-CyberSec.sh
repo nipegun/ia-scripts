67513b959725a0d7d68dc9b316eb1c581ef76bbd
NiPeGun <nipegun@gmail.com>
2025-02-20 18:34:14 +0100

    Update LlamaCPP-CyberSec.sh

--- SoftInst/ParaCli/Modelos/LlamaCPP-CyberSec.sh
+++ SoftInst/ParaCli/Modelos/LlamaCPP-CyberSec.sh
@@ -9,7 +9,7 @@
 # Script de NiPeGun para instalar y configurar los diferentes modelos para ciberseguridad en LlamaCPP para Debian
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Llama.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/LlamaCPP-CyberSec | bash
 # ----------
 
 # Definir constantes de color
@@ -20,6 +20,8 @@
   # Para el color rojo también:
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
+
+vCarpetaDeModelosGGUF="/Particiones/local-lvm/IA/Modelos/GGUF/"
 
 # Indicar inicio de ejecución del script
   echo ""
@@ -38,29 +40,22 @@
     fi
   menu=(dialog --checklist "Marca los modelos que quieras instalar:" 22 96 16)
     opciones=(
+      1 "TheBloke/WhiteRabbitNeo-13B-GGUF    (Q8_0)                      (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+      2 "TheBloke/WhiteRabbitNeo-33B-v1-GGUF (Q8_0)                      (x,x GB en disco) (x,x GB en RAM/VRAM)" off
 
-      1 "llama3.3   70b-instruct-q4_0 (x GB en disco) (x,x GB en RAM/VRAM)" off
-      2 "llama3.3   70b-instruct-q8_0 (x GB en disco) (x,x GB en RAM/VRAM)" off
-      3 "llama3.3   70b-instruct-fp16 (x GB en disco) (x,x GB en RAM/VRAM)" off
+      3 "bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF (7B-Q4_K_M) (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+      4 "bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF (7B-Q8_0)   (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+      5 "bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF (7B-f16)    (x,x GB en disco) (x,x GB en RAM/VRAM)" off
 
+      6 "bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF (7B-Q4_K_M)        (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+      7 "bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF (7B-Q8_0)          (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+      8 "bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF (7B-f16)           (x,x GB en disco) (x,x GB en RAM/VRAM)" off
 
-      4 "llama3.2   1b-instruct-q4_0 (0,9 GB en disco) (1,2 GB en RAM/VRAM)" off
-      5 "llama3.2   1b-instruct-q8_0 (1,5 GB en disco) (1,7 GB en RAM/VRAM)" off
-      6 "llama3.2   1b-instruct-fp16 (2,7 GB en disco) (2,8 GB en RAM/VRAM)" off
+      9 "bartowski/Llama-3.1-WhiteRabbitNeo-2-70B-GGUF (Q4_K_M)          (x,x GB en disco) (x,x GB en RAM/VRAM)" off
 
-      7 "llama3.2   3b-instruct-q4_0 (2,0 GB en disco) (2,9 GB en RAM/VRAM)" off
-      8 "llama3.2   3b-instruct-q8_0 (3,6 GB en disco) (4,4 GB en RAM/VRAM)" off
-      9 "llama3.2   3b-instruct-fp16 (6,6 GB en disco) (7,3 GB en RAM/VRAM)" off
-
-
-     10 "llama3.1   8b-instruct-q4_0 ( 4,8 GB en disco) ( 5,7 GB en RAM/VRAM)" off
-     11 "llama3.1   8b-instruct-q8_0 ( 8,6 GB en disco) ( 9,4 GB en RAM/VRAM)" off
-     12 "llama3.1   8b-instruct-fp16 (16,2 GB en disco) (16,7 GB en RAM/VRAM)" off
-
-     13 "llama3.1 405b-instruct-q4_0 (230,0 GB en disco) (x,x GB en RAM/VRAM)" off
-     14 "llama3.1 405b-instruct-q8_0 (433,0 GB en disco) (x,x GB en RAM/VRAM)" off
-     15 "llama3.1 405b-instruct-fp16 (815,0 GB en disco) (x,x GB en RAM/VRAM)" off
-
+     10 "mradermacher/Trinity-13B-GGUF    (Q4_K_M)                       (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+     11 "mradermacher/Trinity-13B-GGUF    (Q8_0)                         (x,x GB en disco) (x,x GB en RAM/VRAM)" off
+     12 "mradermacher/Trinity-13B-i1-GGUF (i1-Q6_K)                      (x,x GB en disco) (x,x GB en RAM/VRAM)" off
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
@@ -68,25 +63,25 @@
       do
         case $choice in
 
+      sudo mkdir -p "$vCarpetaDeModelosGGUF"
+      sudo chown $USER:$USER "$vCarpetaDeModelosGGUF"
+      cd "$vCarpetaDeModelosGGUF"
+      sudo curl -L https://huggingface.co/TheBloke/WhiteRabbitNeo-13B-GGUF/resolve/main/whiterabbitneo-13b.Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/TheBloke/WhiteRabbitNeo-33B-v1-GGUF/resolve/main/whiterabbitneo-33b-v1.Q8_0.gguf -O
 
-  curl -L https://huggingface.co/TheBloke/WhiteRabbitNeo-13B-GGUF/resolve/main/whiterabbitneo-13b.Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-Q4_K_M.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-f16.gguf -O
 
-  curl -L https://huggingface.co/TheBloke/WhiteRabbitNeo-33B-v1-GGUF/resolve/main/whiterabbitneo-33b-v1.Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-Q4_K_M.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-f16.gguf -O
 
-  curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-Q4_K_M.gguf -O
-  curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-Q8_0.gguf -O
-  curl -L https://huggingface.co/bartowski/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-GGUF/resolve/main/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B-f16.gguf -O
+      sudo curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-70B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-70B-Q4_K_M.gguf -O
 
-  curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-Q4_K_M.gguf -O
-  curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-Q8_0.gguf -O
-  curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-8B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-8B-f16.gguf -O
-
-  curl -L https://huggingface.co/bartowski/Llama-3.1-WhiteRabbitNeo-2-70B-GGUF/resolve/main/Llama-3.1-WhiteRabbitNeo-2-70B-Q4_K_M.gguf -O
-
-  curl -L https://huggingface.co/mradermacher/Trinity-13B-GGUF/resolve/main/Trinity-13B.Q4_K_M.gguf -O
-  curl -L https://huggingface.co/mradermacher/Trinity-13B-GGUF/resolve/main/Trinity-13B.Q8_0.gguf -O
-
-  curl -L https://huggingface.co/mradermacher/Trinity-13B-i1-GGUF/resolve/main/Trinity-13B.i1-Q6_K.gguf -O
+      sudo curl -L https://huggingface.co/mradermacher/Trinity-13B-GGUF/resolve/main/Trinity-13B.Q4_K_M.gguf -O
+      sudo curl -L https://huggingface.co/mradermacher/Trinity-13B-GGUF/resolve/main/Trinity-13B.Q8_0.gguf -O
+      sudo curl -L https://huggingface.co/mradermacher/Trinity-13B-i1-GGUF/resolve/main/Trinity-13B.i1-Q6_K.gguf -O
 
 
   
