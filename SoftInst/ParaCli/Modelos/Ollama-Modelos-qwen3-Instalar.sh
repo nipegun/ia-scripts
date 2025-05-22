7baf9e0ac8ec97d994be9bb7c2f38877cdf785b1
NiPeGun <nipegun@gmail.com>
2025-05-22 11:31:01 +0200

    Update Ollama-Modelos-qwen3-Instalar.sh

--- SoftInst/ParaCli/Modelos/Ollama-Modelos-qwen3-Instalar.sh
+++ SoftInst/ParaCli/Modelos/Ollama-Modelos-qwen3-Instalar.sh
@@ -110,37 +110,37 @@
   menu=(dialog --checklist "Marca los modelos que quieras instalar:" 22 80 16)
     opciones=(
 
-       1 "qwen3:0.6b-q4_K_M ( 0,6 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       2 "qwen3:0.6b-q8_0   ( 0,9 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       3 "qwen3:0.6b-fp16   ( 1,6 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+       1 "qwen3:0.6b-q4_K_M ( 0,6 GB en disco) (0,0 GB en RAM/VRAM)" off
+       2 "qwen3:0.6b-q8_0   ( 0,9 GB en disco) (0,0 GB en RAM/VRAM)" off
+       3 "qwen3:0.6b-fp16   ( 1,6 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-       4 "qwen3:1.7b-q4_K_M ( 1,5 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       5 "qwen3:1.7b-q8_0   ( 2,3 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       6 "qwen3:1.7b-fp16   ( 4,2 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+       4 "qwen3:1.7b-q4_K_M ( 1,5 GB en disco) (0,0 GB en RAM/VRAM)" off
+       5 "qwen3:1.7b-q8_0   ( 2,3 GB en disco) (0,0 GB en RAM/VRAM)" off
+       6 "qwen3:1.7b-fp16   ( 4,2 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-       7 "qwen3:4b-q4_K_M   ( 2,7 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       8 "qwen3:4b-q8_0     ( 4,5 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-       9 "qwen3:4b-fp16     ( 8,2 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+       7 "qwen3:4b-q4_K_M   ( 2,7 GB en disco) (0,0 GB en RAM/VRAM)" off
+       8 "qwen3:4b-q8_0     ( 4,5 GB en disco) (0,0 GB en RAM/VRAM)" off
+       9 "qwen3:4b-fp16     ( 8,2 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-      10 "qwen3:8b-q4_K_M   ( 5,3 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      11 "qwen3:8b-q8_0     ( 9,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      12 "qwen3:8b-fp16     (16,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      10 "qwen3:8b-q4_K_M   ( 5,3 GB en disco) (0,0 GB en RAM/VRAM)" off
+      11 "qwen3:8b-q8_0     ( 9,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      12 "qwen3:8b-fp16     (16,1 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-      13 "qwen3:14b-q4_K_M  ( 9,4 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      14 "qwen3:14b-q8_0    (15,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      15 "qwen3:14b-fp16    ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      13 "qwen3:14b-q4_K_M  ( 9,4 GB en disco) (0,0 GB en RAM/VRAM)" off
+      14 "qwen3:14b-q8_0    (15,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      15 "qwen3:14b-fp16    (29,1 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-      16 "qwen3:30b-a3b-q4_K_M ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      17 "qwen3:30b-a3b-q8_0   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      18 "qwen3:30b-a3b-fp16   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      16 "qwen3:30b-a3b-q4_K_M (18,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      17 "qwen3:30b-a3b-q8_0   (32,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      18 "qwen3:30b-a3b-fp16   (61,1 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-      19 "qwen3:32b-q4_K_M ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      20 "qwen3:32b-q8_0   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      21 "qwen3:32b-fp16   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      19 "qwen3:32b-q4_K_M (20,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      20 "qwen3:32b-q8_0   (35,1 GB en disco) (0,0 GB en RAM/VRAM)" off
+      21 "qwen3:32b-fp16   (65,1 GB en disco) (0,0 GB en RAM/VRAM)" off
 
-      22 "qwen3:235b-a22b-q4_K_M ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      23 "qwen3:235b-a22b-q8_0   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
-      24 "qwen3:235b-a22b-fp16   ( 0,0 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      22 "qwen3:235b-a22b-q4_K_M (142,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      23 "qwen3:235b-a22b-q8_0   (250,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
+      24 "qwen3:235b-a22b-fp16   (470,1 GB en disco) ( 0,0 GB en RAM/VRAM)" off
 
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
@@ -208,7 +208,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=1.6
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -234,7 +234,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=1.5
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -260,7 +260,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=2.3
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -286,7 +286,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=4.2
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -312,7 +312,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=2.7
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -338,7 +338,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=4.5
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -364,7 +364,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=8.2
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -390,7 +390,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=5.3
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -416,7 +416,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=9.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -442,7 +442,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=16.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -468,7 +468,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=9.4
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -494,7 +494,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=15.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -520,7 +520,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=29.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -546,7 +546,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=18.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -572,7 +572,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=32.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -598,7 +598,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=61.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -624,7 +624,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=20.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -650,7 +650,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=35.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -676,7 +676,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=65.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -702,7 +702,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=142.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -728,7 +728,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=250.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
@@ -754,7 +754,7 @@
             echo ""
 
             # Definir el espacio que ocupa el modelo
-              vEspacioQueOcupaElModelo=0
+              vEspacioQueOcupaElModelo=470.1
 
             # Calcular espacio libre disponible antes de instalar el modelo
               if fCalcularEspacioLibreEnCarpetaDeModelos $vEspacioQueOcupaElModelo; then
