025a40aabff1d1580db2484eb9aa614e2c2b2338
NiPeGun <nipegun@gmail.com>
2025-07-10 22:25:03 +0200

    Update Ollama-Modelos-CyberSec.sh

--- SoftInst/ParaCLI/Modelos/Ollama-Modelos-CyberSec.sh
+++ SoftInst/ParaCLI/Modelos/Ollama-Modelos-CyberSec.sh
@@ -10,7 +10,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar los diferentes modelos de ciberseguridad en Ollama para Debian
+# Script de NiPeGun para instalar y configurar los diferentes modelos de ciberseguridad para Ollama en Debian
 #
 # Ejecución remota (Puede requerir permisos sudo):
 #   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Modelos-CyberSec.sh | bash
