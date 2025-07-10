424d7845f2788966d1c616ed1d939b9a7a3d47bc
NiPeGun <nipegun@gmail.com>
2025-07-10 15:33:31 +0200

    Update Ollama-Hacking-WhiteRabbitNeo.sh

--- SoftInst/ParaCLI/Modelos/Ollama-Hacking-WhiteRabbitNeo.sh
+++ SoftInst/ParaCLI/Modelos/Ollama-Hacking-WhiteRabbitNeo.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar los diferentes modelos de WhiteRabbitNeo en Ollama para Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Qwen.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Hacking-WhiteRabbitNeo.sh | bash
 #
 # Ejecución remota como root:
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Qwen.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Hacking-WhiteRabbitNeo.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
