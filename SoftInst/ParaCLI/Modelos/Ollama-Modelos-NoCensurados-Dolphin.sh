35075b2e1c272a11ad9253f6bb1509d7ce47fdca
NiPeGun <nipegun@gmail.com>
2025-07-10 22:25:42 +0200

    Update Ollama-Modelos-NoCensurados-Dolphin.sh

--- SoftInst/ParaCLI/Modelos/Ollama-Modelos-NoCensurados-Dolphin.sh
+++ SoftInst/ParaCLI/Modelos/Ollama-Modelos-NoCensurados-Dolphin.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar los diferentes modelos de Dolphin Uncensored en Ollama para Debian
+# Script de NiPeGun para instalar y configurar los diferentes modelos no censurados de Dolphin para Ollama en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
 #   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Modelos-NoCensurados-Dolphin.sh | bash
