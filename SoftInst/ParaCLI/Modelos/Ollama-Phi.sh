7232330a10932753b33fecbc0c750b53ab34d08c
NiPeGun <nipegun@gmail.com>
2025-07-10 15:35:24 +0200

    Update Ollama-Phi.sh

--- SoftInst/ParaCLI/Modelos/Ollama-Phi.sh
+++ SoftInst/ParaCLI/Modelos/Ollama-Phi.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar los diferentes modelos de Phi en Ollama para Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Phi.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Phi.sh | bash
 #
 # Ejecución remota (como root):
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Phi.sh | 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Phi.sh | 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
