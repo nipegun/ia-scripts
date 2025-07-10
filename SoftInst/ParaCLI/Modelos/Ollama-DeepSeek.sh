b1b6843a2ab3b7d924565b7c1616a25b6a8a7dff
NiPeGun <nipegun@gmail.com>
2025-07-10 15:32:37 +0200

    Update Ollama-DeepSeek.sh

--- SoftInst/ParaCLI/Modelos/Ollama-DeepSeek.sh
+++ SoftInst/ParaCLI/Modelos/Ollama-DeepSeek.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar los diferentes modelos de DeepSeek en Ollama para Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Qwen.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-DeepSeek.sh | bash
 #
 # Ejecución remota como root:
-#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCli/Modelos/Ollama-Qwen.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-DeepSeek.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
