afd27e6ba7cbee8942497ee864a043ba58ba9a2d
NiPeGun <nipegun@gmail.com>
2025-09-02 05:02:44 +0200

    Create TextGenerationWebUI-Llama.sh

---
+++ SoftInst/ParaCLI/Modelos/TextGenerationWebUI-Llama.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar los diferentes modelos LLM de Llama para Ollama en Debian
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/ia-scripts/refs/heads/main/SoftInst/ParaCLI/Modelos/Ollama-Modelos-LLM-Llama.sh | bash
+# ----------
+
+# Definir la carpeta de los modelos
+  vCarpeta="/opt/text-generation-webui/models/"
+
+# 
