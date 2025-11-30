4e52dc72eb81191badc9867b012a7e2e57096a45
NiPeGun <nipegun@gmail.com>
2025-11-30 02:58:24 +0100

    Update LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh

--- SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
+++ SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
@@ -2,6 +2,11 @@
 
 cd $HOME/IA/Modelos/GGUF/
 curl -L https://huggingface.co/bartowski/dolphincoder-starcoder2-15b-GGUF/resolve/main/dolphincoder-starcoder2-15b-Q8_0.gguf -O
-
 # Servir
   # $HOME/IA/LlamaCPP/llama-server -m $HOME/IA/Modelos/GGUF/dolphincoder-starcoder2-15b-Q8_0.gguf --port 9000
+
+
+cd $HOME/IA/Modelos/GGUF/
+curl -L https://huggingface.co/bartowski/dolphincoder-starcoder2-15b-GGUF/resolve/main/dolphincoder-starcoder2-15b-Q4_K_M.gguf -O
+# Servir
+  # $HOME/IA/LlamaCPP/llama-server -m $HOME/IA/Modelos/GGUF/dolphincoder-starcoder2-15b-Q4_K_M.gguf --port 9000
