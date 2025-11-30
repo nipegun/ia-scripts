d486a69efe52e8d7080ab04902893c0c5fa16f9e
NiPeGun <nipegun@gmail.com>
2025-11-30 04:00:23 +0100

    Update LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh

--- SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
+++ SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
@@ -10,3 +10,12 @@ cd $HOME/IA/Modelos/GGUF/
 curl -L https://huggingface.co/bartowski/dolphincoder-starcoder2-15b-GGUF/resolve/main/dolphincoder-starcoder2-15b-Q4_K_M.gguf -O
 # Servir
   # $HOME/IA/LlamaCPP/llama-server -m $HOME/IA/Modelos/GGUF/dolphincoder-starcoder2-15b-Q4_K_M.gguf --port 9000
+
+
+# Cyber
+cd $HOME/IA/Modelos/GGUF/
+curl -L https://huggingface.co/segolilylabs/Lily-Cybersecurity-7B-v0.2-GGUF/resolve/main/Lily-7B-Instruct-v0.2.Q5_K_M.gguf -O
+
+https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-v2-70B-Q4_K_M/resolve/main/Llama-3.3-70B-Instruct-Cybersecurity-Q4_K_M.gguf
+https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-Qwen3-32B-Q8_0-GGUF/resolve/main/senecallm-x-qwen3-32b-q8_0.gguf
+https://huggingface.co/AlicanKiraz0/Seneca-Cybersecurity-LLM-Q4_K_M-GGUF/resolve/main/senecallm-q4_k_m.gguf
