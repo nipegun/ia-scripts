3690223b07606c812a53ba4c1b2eb879a9d9db78
NiPeGun <nipegun@gmail.com>
2025-12-01 03:52:02 +0100

    Update LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh

--- SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
+++ SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
@@ -19,3 +19,14 @@ curl -L https://huggingface.co/segolilylabs/Lily-Cybersecurity-7B-v0.2-GGUF/reso
 https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-v2-70B-Q4_K_M/resolve/main/Llama-3.3-70B-Instruct-Cybersecurity-Q4_K_M.gguf
 https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-Qwen3-32B-Q8_0-GGUF/resolve/main/senecallm-x-qwen3-32b-q8_0.gguf
 https://huggingface.co/AlicanKiraz0/Seneca-Cybersecurity-LLM-Q4_K_M-GGUF/resolve/main/senecallm-q4_k_m.gguf
+
+curl -L https://huggingface.co/ggml-org/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-mxfp4.gguf -O
+
+curl -L https://huggingface.co/ggml-org/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q4_K_M.gguf -O
+curl -L https://huggingface.co/ggml-org/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q8_0.gguf -O
+
+curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q4_K_M.gguf -O
+curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q8_0.gguf -O
+curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-f16.gguf -O
+
+
