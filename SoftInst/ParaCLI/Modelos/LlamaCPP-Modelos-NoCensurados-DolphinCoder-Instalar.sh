abea9dad069a40984edfcbe163af4e04c8388f78
NiPeGun <nipegun@gmail.com>
2025-12-08 01:25:17 +0100

    Update LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh

--- SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
+++ SoftInst/ParaCLI/Modelos/LlamaCPP-Modelos-NoCensurados-DolphinCoder-Instalar.sh
@@ -13,34 +13,18 @@ curl -L https://huggingface.co/bartowski/dolphincoder-starcoder2-15b-GGUF/resolv
 
 
 # Cyber
-mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
-cd $HOME/IA/Modelos/GGUF/
-curl -L https://huggingface.co/segolilylabs/Lily-Cybersecurity-7B-v0.2-GGUF/resolve/main/Lily-7B-Instruct-v0.2.Q5_K_M.gguf -O
 
-https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-v2-70B-Q4_K_M/resolve/main/Llama-3.3-70B-Instruct-Cybersecurity-Q4_K_M.gguf
+  # Lily
+    mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
+    cd $HOME/IA/Modelos/GGUF/
+    curl -L https://huggingface.co/segolilylabs/Lily-Cybersecurity-7B-v0.2-GGUF/resolve/main/Lily-7B-Instruct-v0.2.Q5_K_M.gguf -O
+
+  # DeepHat
+    mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
+    cd $HOME/IA/Modelos/GGUF/
+    curl -L https://huggingface.co/mradermacher/DeepHat-V1-7B-GGUF/resolve/main/DeepHat-V1-7B.f16.gguf -O
+
+      https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-v2-70B-Q4_K_M/resolve/main/Llama-3.3-70B-Instruct-Cybersecurity-Q4_K_M.gguf
 https://huggingface.co/Trendyol/Trendyol-Cybersecurity-LLM-Qwen3-32B-Q8_0-GGUF/resolve/main/senecallm-x-qwen3-32b-q8_0.gguf
-https://huggingface.co/AlicanKiraz0/Seneca-Cybersecurity-LLM-Q4_K_M-GGUF/resolve/main/senecallm-q4_k_m.gguf
+      https://huggingface.co/AlicanKiraz0/Seneca-Cybersecurity-LLM-Q4_K_M-GGUF/resolve/main/senecallm-q4_k_m.gguf
 
-curl -L https://huggingface.co/ggml-org/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-mxfp4.gguf -O
-
-curl -L https://huggingface.co/ggml-org/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q4_K_M.gguf -O
-curl -L https://huggingface.co/ggml-org/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q8_0.gguf -O
-
-curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q4_K_M.gguf -O
-curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q8_0.gguf -O
-curl -L https://huggingface.co/ggml-org/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-f16.gguf -O
-
-
-# Gemma 3 12b
-
-mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
-cd $HOME/IA/Modelos/GGUF/
-curl -L https://huggingface.co/ggml-org/gemma-3-12b-it-GGUF/resolve/main/gemma-3-12b-it-Q4_K_M.gguf -O
-
-mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
-cd $HOME/IA/Modelos/GGUF/
-curl -L https://huggingface.co/ggml-org/gemma-3-12b-it-GGUF/resolve/main/gemma-3-12b-it-Q8_0.gguf -O
-
-mkdir -p $HOME/IA/Modelos/GGUF/ 2> /dev/null
-cd $HOME/IA/Modelos/GGUF/
-curl -L https://huggingface.co/ggml-org/gemma-3-12b-it-GGUF/resolve/main/gemma-3-12b-it-f16.gguf -O
